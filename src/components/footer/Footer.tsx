c5836a4c43e7354e7ec11d38944c5595dc6e0358
Sourav Panda <pandasourav140@gmail.com>
2024-03-14 01:50:18 +0530

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>ENTNT</span>
+      <span>© ENTNT ERP Software</span>
+    </div>
+  )
+}
+
+export default Footer
